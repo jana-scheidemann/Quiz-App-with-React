e4b65a06dd0571684d3c0034a2f3dda01b230e92
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-05-26 20:16:17 +0200

    create new quiz

--- styles.js
+++ styles.js
@@ -7,17 +7,47 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  :root {
-  --color-light: rgb(151, 212, 151);
-  --color-medium: rgb(69, 138, 69);
-  --color-dark: rgb(28, 78, 28);
-  --color-foam: rgb(242, 242, 242);
-  --color-darkMode: rgb(52, 52, 52);
-  --general-border-radius: 5px;
-}
-
   body {
     margin: 0;
-    padding: 20px;
+    background: #0C0C0C;
+    font-family: Verdana;
+    line-height: 1.3;
+  }
+
+  h1 {
+    font-weight: 200;
+    color: #FF0080;
+    margin: 20px 0 0 0;
+    text-align: center;
+  }
+
+  ul {
+    padding-inline-start: 0;
+  }
+
+  li {
+    list-style-type: none;
+    margin:  0;
+    background: #007F73;
+    color: #FFF455;
+    width: 100%;
+    padding: 5% 15%;
+    margin: 10% 0;
+    font-size: 20px;
+  }
+
+  .answer {
+    font-style: italic;
+    font-size: 16px;
+    text-align: right;
+  }
+
+  button {
+    background: #0C0C0C;
+    color: #FF0080;
+    padding: 10px;
+    border-radius: 20px;
+    width: 150px;
+    border: none;
   }
 `;
